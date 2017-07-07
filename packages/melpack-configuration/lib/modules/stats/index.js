2f6db7f18c730493a6576c90b370e695ac95894f
dadviegas <david.viegas@talkdesk.com>
2017-07-07 15:28:15 +0100

    Update webpack version

--- packages/melpack-configuration/lib/modules/stats/index.js
+++ packages/melpack-configuration/lib/modules/stats/index.js
@@ -4,19 +4,13 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _webpackMerge = require('webpack-merge');
-
-var _webpackMerge2 = _interopRequireDefault(_webpackMerge);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
 exports.default = function (options) {
   return function () {
     var setup = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
 
     // read more on https://webpack.js.org/configuration/stats/
     // options could be an object or string
-    setup.build = (0, _webpackMerge2.default)(setup.build, { stats: options || 'normal' });
+    setup.build = setup.merge(setup.build, { stats: options || 'normal' });
     return setup;
   };
 };
--- packages/melpack-configuration/src/modules/stats/index.js
+++ packages/melpack-configuration/src/modules/stats/index.js
@@ -1,8 +1,6 @@
-import merge from 'webpack-merge'
-
 export default (options) => (setup = {}) => {
   // read more on https://webpack.js.org/configuration/stats/
   // options could be an object or string
-  setup.build = merge(setup.build, { stats: options || 'normal' })
+  setup.build = setup.merge(setup.build, { stats: options || 'normal' })
   return setup
 }

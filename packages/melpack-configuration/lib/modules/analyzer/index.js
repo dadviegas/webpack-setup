6a4231f15b7521f4e7a11442edb48fcda8891710
dadviegas <david.viegas@talkdesk.com>
2017-07-07 10:58:30 +0100

    change analyzer

--- packages/melpack-configuration/lib/modules/analyzer/index.js
+++ packages/melpack-configuration/lib/modules/analyzer/index.js
@@ -6,19 +6,13 @@ Object.defineProperty(exports, "__esModule", {
 
 var _webpackBundleAnalyzer = require('webpack-bundle-analyzer');
 
-var _webpackMerge = require('webpack-merge');
-
-var _webpackMerge2 = _interopRequireDefault(_webpackMerge);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
 exports.default = function (options) {
   return function (setup) {
     var configuration = { plugins: [] };
 
     if (setup.analyzer) {
       configuration.plugins.push(new _webpackBundleAnalyzer.BundleAnalyzerPlugin(options));
-      setup.build = (0, _webpackMerge2.default)(setup.build, configuration);
+      setup.build = setup.merge(setup.build, configuration);
     }
 
     return setup;
--- packages/melpack-configuration/src/modules/analyzer/index.js
+++ packages/melpack-configuration/src/modules/analyzer/index.js
@@ -1,12 +1,11 @@
 import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer'
-import merge from 'webpack-merge'
 
 export default (options) => (setup) => {
   const configuration = {plugins: []}
 
   if (setup.analyzer) {
     configuration.plugins.push(new BundleAnalyzerPlugin(options))
-    setup.build = merge(setup.build, configuration)
+    setup.build = setup.merge(setup.build, configuration)
   }
 
   return setup
